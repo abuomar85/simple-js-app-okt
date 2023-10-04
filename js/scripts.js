b8c8bf550ac8e9bfea770928b6994c2fc48b7376
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2023-10-04 16:49:15 +0200

    update js file
    looping through the pokemonsList and using if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-let pokemonList = [
+ // creating a list of pokemons
+ let pokemonList = [
+    
     {name: 'balbasure', height: 7, types: ['grass', 'poison']},
+    {name:'charizard', height:17, types:['fire','flying ']},
     {name:'charmander', height: 6, types: ['fire']},
-    {name:'charizard', height:17, types:['fire','flying ']}
+   
 ];
+ 
+// looping through the pokemon list and type the results on the html page
+
+for(let i=0; i<pokemonList.length; i++) {
+    if(pokemonList[i].height > 7){    
+   document.write( (pokemonList[i].name + ' height ( ' + pokemonList[i].height + ') Wow that is big <br>'))
+    }
+    else{
+        document.write((pokemonList[i].name + ' height ( ' + pokemonList[i].height + ' ) <br>'))
+    }
+}
