c172fc1b0db19e5fca36243be15334dff11e04f6
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2023-10-09 15:17:49 +0200

    update html css and js files
    1- add polyfills and fetch to html
     2- update the pokemonlist to load the data from api
    3- adding new functions :
    load list to load the list of pokemons
    load details do display the pokemon details in console

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,12 @@
     let pokemonRepository = (function() {
 
         // create the pokeomnList
-        let pokemonList = [
-        
-            {name: 'balbasure', height: 7, types: ['grass', 'poison']},
-            {name:'charizard', height:17, types:['fire','flying ']},
-            {name:'charmander', height: 6, types: ['fire']},
-        
-        ];
+        let pokemonList = [];
 
-        // get all pokemons
-        function getAll() {
-            return pokemonList;
-        }
+        let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=20';
 
-        // check if the new pokemon is an object and has the whole keys before adding it
         function add(pokemon){
-            if(typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'types' in pokemon) {
+            if(typeof pokemon === 'object' && 'name' in pokemon ) {
                 return pokemonList.push(pokemon)
                
             } else {
@@ -26,6 +16,46 @@
             return document.write('<p> not object</p>')
             }
         }
+
+
+
+        // get all pokemons
+        function getAll() {
+            return pokemonList;
+        }
+        // check if the new pokemon is an object and has the whole keys before adding it
+
+        function loadList() {
+            return fetch(apiUrl).then(function(response) {
+                return response.json();
+            }).then(function(json) {
+                json.results.forEach(function(item) {
+                    let pokemon = {
+                        name: item.name,
+                        detailsUrl: item.url
+                    };
+                    add(pokemon);
+                    console.log(pokemon);
+                });
+            }).catch(function(e) {
+                console.error(e)
+            })
+        }
+                // load the pokemon details
+                function loadDetails(item) {
+                    let url = item.detailsUrl;
+                    return fetch(url).then(function(response) {
+                        return response.json();
+                    }).then(function(details) {
+                        // add the details to the item 
+                        item.imageUrl = details.sprites.fron_default;
+                        item.height = details.height;
+                        item.types = details.types;
+                    }).catch(function(e) {
+                        console.error(e)
+                    })
+        
+                }
 
 
         // filter the pokemons by the name
@@ -42,18 +72,24 @@
             button.classList.add('pokemon-button');
             pokemonList.appendChild(listItem); 
             listItem.appendChild(button);
-            // log the pokemon name when the button is clicked 
+            // log the pokemon name when the button is clicked
             showDetails(button,pokemon)
             
         }
 
         //listen to the events and log the pokemon name when the button is clicked
-        function showDetails(button, pokemon) {
+        function showDetails(button, pokemon) {   
+            let container = document.querySelector('.container');  
             button.addEventListener('click', function() {
-                console.log(pokemon.name);
-            })
-            
+                loadDetails(pokemon).then(function() {
+                    console.log(pokemon);
+                    container.innerText = (pokemon.name);  
+                })
+                
+             });   
         }
+
+
 
 
         return{
@@ -61,7 +97,10 @@
             getAll: getAll,
            findPokemonByName: findPokemonByName,
            addListItem: addListItem,
-           showDetails: showDetails
+           showDetails: showDetails,
+           loadList:loadList,
+           loadDetails: loadDetails
+           
             
         }
 
@@ -75,14 +114,20 @@
     } else {
         console.log('Pokemon not found.');
     } 
-    //adding new pokemon 
-    pokemonRepository.add({name: 'Squirtle', height: 5, types: ['Squirtle']});
-
+    //adding new pokemon // since we add the pokemon manually it will generate an error in console when it is been clicked 
+   // pokemonRepository.add({name: 'Squirtle', height: 5, types: ['Squirtle']});
+    // load data from api 
+    pokemonRepository.loadList().then(function() {
+        pokemonRepository.getAll().forEach(function (pokemon) {
+            pokemonRepository.addListItem(pokemon);
+        });
+    });
     //looping throug the pokemons using forEachLoop
-    pokemonRepository.getAll().forEach(function (pokemon) {
-        pokemonRepository.addListItem(pokemon);
-    }) 
+ 
 
+
+
+   
 
     
 
